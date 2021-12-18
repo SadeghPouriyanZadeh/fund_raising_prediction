a655fb543edd82541ad9be61f58e8a246b99ac02
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2021-12-18 17:10:53 +0330

    data module class edited to IcoDataset

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ from torch.utils.data import Dataset
 from pandas import DataFrame
 import numpy as np
 
-class CancerDataset(Dataset):
+class IcoDataset(Dataset):
     def __init__(self, df: DataFrame):
-        self.df_x = df.drop(columns=["lung_cancer"])
-        self.df_y = df["lung_cancer"]
+        self.df_x = df.drop(columns=["Total amount raised (USDm)"])
+        self.df_y = df["Total amount raised (USDm)"]
         self._length = len(self.df_x)
     def __len__(self):
         return self._length
