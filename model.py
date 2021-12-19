bd2a87e2254c86300d05de76b9fdaaf34d892651
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2021-12-19 15:40:56 +0330

    implement Kfold + change model to rectangular fcc

--- model.py
+++ model.py
@@ -1,14 +1,20 @@
 from torch import nn
 
+class LinearReLU(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        return self.relu(self.linear(x))
+
 class IcoPredictor(nn.Module):
-    def __init__(self, features, shallow_nodes) -> None:
+    def __init__(self, features, hidden_layers, layer_units) -> None:
         super().__init__()
         self.regressor = nn.Sequential(
-            nn.Linear(features, shallow_nodes),
-            nn.ReLU(),
-            nn.Linear(shallow_nodes, 1),
-            nn.ReLU(),
-
+            LinearReLU(features, layer_units),
+           *[LinearReLU(layer_units, layer_units) for _ in range(hidden_layers)],
+            LinearReLU(layer_units, 1)
         )
     def forward(self, x):
         return self.regressor(x)
