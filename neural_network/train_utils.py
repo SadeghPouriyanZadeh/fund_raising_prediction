3eceb70f559aa859ffb6704cc9c4280ef436a4dc
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2021-12-23 03:00:12 +0330

    move 2 functions and 1 class to params_utils

--- neural_network/train_utils.py
+++ neural_network/train_utils.py
@@ -1,13 +1,9 @@
-from itertools import product
-
 import numpy as np
-import pandas as pd
 import torch
-from pandas import DataFrame
+from data import IcoDataset, get_processed_data
 from sklearn.model_selection import KFold
 from torch.utils.data import DataLoader
 
-from data import IcoDataset, get_processed_data
 from neural_network.model import IcoPredictor
 
 
@@ -92,64 +88,3 @@ def run_with_kfold(
         total_val_losses.append(min(val_losses))
     val_loss = np.sqrt(np.array(total_val_losses).mean())
     return val_loss
-
-
-def param_generator():
-    params_values = {
-        "batch_size": [2 ** i for i in range(5, 7)],
-        "hidden_layers": [2 ** i for i in range(2, 4)],
-        "layer_units": [2 ** i for i in range(4, 7)],
-        "learning_rate": np.random.uniform(1e-5, 0.1, 3).tolist(),
-        "normalize": [True, False],
-        "one_hot_encode": [True, False],
-    }
-    keys = [
-        "batch_size",
-        "hidden_layers",
-        "layer_units",
-        "learning_rate",
-        "normalize",
-        "one_hot_encode",
-    ]
-    for row in product(*[params_values[key] for key in keys]):
-        data = {}
-        for i, key in enumerate(keys):
-            data[key] = row[i]
-        yield data
-
-
-class HyperParameterLogger:
-    def __init__(
-        self,
-        csv_file_path,
-    ):
-        self.csv_file_path = csv_file_path
-        self.history = {
-            "batch_size": [],
-            "hidden_layers": [],
-            "layer_units": [],
-            "learning_rate": [],
-            "normalize": [],
-            "one_hot_encode": [],
-            "epochs": [],
-            "data_path": [],
-            "device": [],
-            "val_loss": [],
-            "train_time": [],
-        }
-
-    def log(self, param_dict, val_loss, train_time):
-        self.history["val_loss"].append(val_loss)
-        self.history["train_time"].append(train_time)
-        for key in param_dict:
-            self.history[key].append(param_dict[key])
-        df = pd.DataFrame(self.history)
-        df.to_csv(self.csv_file_path, index=False)
-        return df
-
-
-def find_best_hyperparameter(df: DataFrame, val_loss_label: str):
-    min_val_loss = min(df[val_loss_label])
-    min_val_loss_idx = np.where(df[val_loss_label] == min_val_loss)
-    hyper_parameters = df.values[min_val_loss_idx]
-    return hyper_parameters
