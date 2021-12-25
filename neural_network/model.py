87afd8b11f76ca6cb63c85c2635e9cd430ee6181
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2021-12-25 17:23:43 +0330

    update LinearReLU class

--- neural_network/model.py
+++ neural_network/model.py
@@ -1,21 +1,27 @@
 from torch import nn
 
+
 class LinearReLU(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
-        self.linear = nn.Linear(in_features, out_features)
-        nn.BatchNorm1d(out_features),
-        self.relu = nn.ReLU()
+        self.seq = nn.Sequential(
+            nn.Linear(in_features, out_features),
+            nn.BatchNorm1d(out_features),
+            nn.ReLU(),
+        )
+
     def forward(self, x):
-        return self.relu(self.linear(x))
+        return self.seq(x)
+
 
 class IcoPredictor(nn.Module):
     def __init__(self, features, hidden_layers, layer_units) -> None:
         super().__init__()
         self.regressor = nn.Sequential(
             LinearReLU(features, layer_units),
-           *[LinearReLU(layer_units, layer_units) for _ in range(hidden_layers)],
+            *[LinearReLU(layer_units, layer_units) for _ in range(hidden_layers)],
             LinearReLU(layer_units, 1)
         )
+
     def forward(self, x):
         return self.regressor(x)
